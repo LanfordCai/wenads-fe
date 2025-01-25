17e3e11d6d2fcac28d1e857a6a68eee716b4781b
lanford33 <lanfordcai@outlook.com>
2025-01-25 14:04:06 +0800

    fix: error

--- src/app/avatar/components/AvatarEditor.tsx
+++ src/app/avatar/components/AvatarEditor.tsx
@@ -132,15 +132,6 @@ const AvatarEditor: FC<AvatarEditorProps> = ({
     }
   };
 
-  const calculateTotalPrice = () => {
-    const total = Object.values(selectedComponents).reduce(
-      (sum, component) => sum + (component?.price || BigInt(0)),
-      BigInt(0)
-    );
-    // Convert from wei to MON and format with 2 decimal places
-    return (Number(total) / 1e18).toFixed(2);
-  };
-
   const renderPreview = () => {
     // If we have an NFT but no components selected, return null
     if (hasNFT && Object.keys(selectedComponents).length === 0) {
